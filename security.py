6617dc506363213afe3eb0b93a5cbc8cf45f03aa
Lakshay <karadlakshay7@gmail.com>
2026-08-19 02:51:20 +0530

    update security.py

--- security.py
+++ security.py
@@ -1,9 +1,10 @@
 from passlib.context import CryptContext
 import os 
 import uuid
-from jose import JOSEError, jwt
+from jose import JWTError, jwt
 from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
+from typing import Optional
 
 load_dotenv()
 
@@ -53,3 +54,27 @@ def create_refresh_token(user_id : int) -> str:
     }
     
     return jwt.encode(payload,REFRESH_SECRET_KEY,algorithm=ALGORITHM)
+
+
+def verify_access_token(token : str) -> Option[dict]:
+    
+    try:
+        payload = jwt.decode(token,SECRET_KEY, algorithms=[ALGORITHM])
+        if payload.get("type") != "access":
+            return None
+        
+        return payload
+    
+    except JWTError:
+        return None
+    
+def verify_refresh_token(token : str) -> Optional[dict]:
+    try:
+        payload = jwt.decode(token,REFRESH_SECRET_KEY,algorithms=[ALGORITHM])
+        if payload.get("type") != "refresh":
+            return None
+        
+        return payload
+    
+    except JWTError:
+        return None
