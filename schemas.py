c2006ec1a8beb87b65b73bde6e9dace88e3c4d9d
Lakshay <karadlakshay7@gmail.com>
2026-08-19 20:13:48 +0530

    update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, EmailStr, Field, SecretStr, model_validator, field_validator
+from pydantic import BaseModel, EmailStr, Field, SecretStr, model_validator, field_validator, ConfigDict
+from datetime import datetime
+from typing import Optional, Self 
 from enum import Enum
 import re
 
@@ -15,19 +17,15 @@ class SignupRequest(BaseModel):
     full_name : str = Field(max_length=50)
     gender : Gender
     phone_number : str | None = Field(pattern=r"^\+?[1-9]\d{1,14}$", examples=["+919876543210"], default=None)
-    
-    
-    
-    
+   
     @field_validator('password')
-    def validate_passowrd_strenth(cls, v : SecretStr) -> SecretStr:
+    def validate_password_strength(cls, v : SecretStr) -> SecretStr:
         password = v.get_secret_value()
         
         rules = {
-            r'[A-Z]' : 'Password must contain atleast one uppercase latter',
-            r'[a-z]' : 'Password must contain atleast one lowercase latter',
+            r'[A-Z]' : 'Password must contain at least one uppercase letter',
+            r'[a-z]' : 'Password must contain at least one lowercase letter',
             r'\d': 'Password must contain at least one number',
-            r'^.{8,}$': 'Password must be at least 8 characters long',
             r'[!@#$%^&*(),.?":{}|<>]': 'Password must contain at least one special character'
         }
         
@@ -36,16 +34,27 @@ class SignupRequest(BaseModel):
                 raise ValueError(error_message)
         
         return v
-        
-    # Validator 2: Check password MATCH TO SAVE CPU POWER 
+    
     @model_validator(mode="after")
     def check_password_match(self):
         if self.password.get_secret_value() != self.confirm_password.get_secret_value():
             raise ValueError("Passwords do not match")
-        return self
-    
+        return self  
     
 class LoginRequest(BaseModel):
     email : EmailStr = Field(title="Mail", examples=["acb@gmail.com"])
     password : SecretStr = Field(min_length=8, max_length=255)
     
+class UserProfileResponse(BaseModel):
+    full_name : str = Field(default=None)
+    gender : Gender = Field(default=None)
+    
+class UserResponse(BaseModel):
+    id : int  
+    email : EmailStr 
+    phone_number : str  | None = None
+    is_active : bool = True
+    is_verify : bool = True
+    created_at : datetime 
+    profile : Optional[UserProfileResponse]  | None = None
+    model_config = ConfigDict(from_attributes=True)
